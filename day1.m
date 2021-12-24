6edcbbd011e7d20f01ab617d0ad8b4d70c4abb3e
Pete Toshkov <pete@Petes-MacBook-Pro.local>
2021-12-24 22:24:05 +0200

    added part 2 of day 1

--- day1.m
+++ day1.m
@@ -1,14 +1,29 @@
-% dat     131    140    136    135    155    175           ...
-% prev    xxx    131    140    136    135    155    175    ...
-% curr    131    140    136    135    155    175    xxx    ...
-% res      0      1      0      0      1      1      0     ...
-
 dat = importdata("day1.dat");
-FIRST = dat(1);
-LAST = dat(length(dat));
-prev = [FIRST;dat];
-curr = [dat;LAST];
-res = sum(curr>prev);
+
+function res = count_increases(dat)
+	% example
+	% dat         131    140    136    135    155    175       
+	% curr        131    140    136    135    155    175    175
+	% prev        131    131    140    136    135    155    175
+	% curr>prev    0      1      0      0      1      1      0 
+	% res		   3
+	FIRST = dat(1);
+	LAST = dat(length(dat));
+	prev = [FIRST;dat];
+	curr = [dat;LAST];
+	res = sum(curr>prev);
+end
+
+% part 1
+res1 = count_increases(dat);
+
+% part2
+WINDOW = 3;
+LEN = length(dat);
+dat = movmean(dat,WINDOW);
+dat = dat_movmean((WINDOW-1):(LEN-1));
+res2 = count_increases(dat);
 
 % test
-assert(res==1466);
+assert(res1==1466);
+assert(res2==1491);
