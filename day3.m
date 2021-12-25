56bad090d8dc22705fa4d8d146075237ee7ebff7
Pete Toshkov <pete@192.168.68.107>
2021-12-25 02:09:09 +0200

    added solution for day 3 part 2

--- day3.m
+++ day3.m
@@ -1,6 +1,6 @@
 dat = importdata("day3.dat");
 
-function res = find_mostcommon(dat,pos)
+function [res,bits] = find_mostcommon(dat,pos)
 	THRESHOLD = (length(dat)/2);
 	bits = bitget(dat,pos);
 	res = (sum(bits) >= THRESHOLD);
@@ -9,25 +9,44 @@ end
 % part 1
 dat = num2str(dat);
 dat = bin2dec(dat);
-gamma =        num2str(find_mostcommon(dat,12)) ;
-gamma = [gamma,num2str(find_mostcommon(dat,11))];
-gamma = [gamma,num2str(find_mostcommon(dat,10))];
-gamma = [gamma,num2str(find_mostcommon(dat, 9))];
-gamma = [gamma,num2str(find_mostcommon(dat, 8))];
-gamma = [gamma,num2str(find_mostcommon(dat, 7))];
-gamma = [gamma,num2str(find_mostcommon(dat, 6))];
-gamma = [gamma,num2str(find_mostcommon(dat, 5))];
-gamma = [gamma,num2str(find_mostcommon(dat, 4))];
-gamma = [gamma,num2str(find_mostcommon(dat, 3))];
-gamma = [gamma,num2str(find_mostcommon(dat, 2))];
-gamma = [gamma,num2str(find_mostcommon(dat, 1))];
+BITLEN = 12;
+gamma =        num2str(find_mostcommon(dat,BITLEN   )(1)) ;
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 1)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 2)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 3)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 4)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 5)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 6)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 7)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 8)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN- 9)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN-10)(1))];
+gamma = [gamma,num2str(find_mostcommon(dat,BITLEN-11)(1))];
 gamma = bin2dec(gamma);
-epsilon = bitcmp(gamma,12);
+epsilon = bitcmp(gamma,BITLEN);
 res1 = (gamma*epsilon);
 
 % part 2
-res2 = 0;
+pos = BITLEN;
+oxygen = dat;
+while (length(oxygen) > 1)
+	[mostcommon,bits] = find_mostcommon(oxygen,pos);
+	matches = (bits==mostcommon);
+	oxygen = nonzeros(oxygen.*matches);
+	pos -= 1;
+end
+
+pos = BITLEN;
+CO2 = dat;
+while (length(CO2) > 1)
+	[mostcommon,bits] = find_mostcommon(CO2,pos);
+	matches = (bits==(~(mostcommon)));
+	CO2 = nonzeros(CO2.*matches);
+	pos -= 1;
+end
+
+res2 = (oxygen*CO2);
 
 %test
 assert(res1==4174964);
-assert(res2==0);
+assert(res2==4474944);
