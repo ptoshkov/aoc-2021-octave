238a0bc97c966f7a28d9e913db8581312ff3f7ee
Pete Toshkov <petetoshkov@gmail.com>
2021-12-29 14:59:01 +0200

    added solution for day 10 part 2

--- day10.m
+++ day10.m
@@ -2,7 +2,7 @@ dat = importdata("day10.dat");
 WEIGHTS = containers.Map({")","]","}",">"},[3,57,1197,25137]);
 
 function txt = checkline(txt,loc)
-	if ((loc<1) || (loc>length(txt)))
+	if (loc>length(txt))
 		return;
 	end
 	if (((txt(loc)==")") && (txt(loc-1)=="(")) ||
@@ -10,18 +10,28 @@ function txt = checkline(txt,loc)
 		((txt(loc)=="}") && (txt(loc-1)=="{")) ||
 		((txt(loc)==">") && (txt(loc-1)=="<")))
 		txt = [txt(1:(loc-2)),txt((loc+1):length(txt))];
-		txt = checkline(txt,(loc-2));
+		txt = checkline(txt,max(1,(loc-2)));
 	else
 		txt = checkline(txt,(loc+1));
 	end
 end
 
-% part 1
+% parts 1 & 2
 res1 = 0;
+res2 = [];
 for i = 1:length(dat)
 	txt = checkline(dat{i},2);
 	closing = min([strfind(txt,")"),strfind(txt,"]"),strfind(txt,"}"),strfind(txt,">")]);
 	if isempty(closing)
+		tmp = 0;
+		for j = 0:(length(txt)-1)
+			pt = (((txt(length(txt)-j)=="(")*1)+
+				((txt(length(txt)-j)=="[")*2)+
+				((txt(length(txt)-j)=="{")*3)+
+				((txt(length(txt)-j)=="<")*4));
+			tmp = ((5*tmp)+pt);
+		end
+		res2(end+1) = tmp;
 		continue;
 	end
 	closing = txt(closing);
@@ -31,16 +41,13 @@ for i = 1:length(dat)
 			txt = dat{i};
 			txt(closing(j)) = (keys(WEIGHTS){k});
 			txt = checkline(txt,2);
-			if isempty([strfind(txt,")"),strfind(txt,"]"),strfind(txt,"}"),strfind(txt,">")])
-				res1 += WEIGHTS((dat{i}(closing(j))));
-			end
+			txt = [strfind(txt,")"),strfind(txt,"]"),strfind(txt,"}"),strfind(txt,">")];
+			res1 += isempty(txt)*(WEIGHTS((dat{i}(closing(j)))));
 		end
 	end
 end
-
-% part 2
-res2 = 0;
+res2 = (sort(res2)(ceil(length(res2)/2)));
 
 % test
-assert(res1==0);
+assert(387363==387363);
 assert(res2==0);
