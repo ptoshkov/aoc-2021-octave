3bcf111e5baff59e0c420f0a7ddd1f956755bda7
Pete Toshkov <petetoshkov@gmail.com>
2021-12-31 03:39:44 +0200

    added solution for day 12 part 2

--- day12.m
+++ day12.m
@@ -1,38 +1,36 @@
 fid = fopen("day12.dat");dat = textscan(fid,"%s"){1};fclose(fid);
 
-function res = findpath(dat,pos,visited,res)
+function res = findpath(dat,pos,visited,res,part1)
 	if strcmp(pos,"end")
-		res = res + 1;
+		res += 1;
 		return;
+	end
+	if (strcmp(pos,"start")||
+			(strcmp(pos,lower(pos))&&
+				((sum(strcmp(cellfun("lower",visited,"UniformOutput",false),visited))>1)||part1)))
+		visited = [visited;pos];
+	elseif (strcmp(pos,lower(pos))&&any(strcmp(cellfun("lower",visited,"UniformOutput",false),pos))&&(~(part1)))
+		visited = cellfun("lower",visited,"UniformOutput",false);
+	elseif (strcmp(pos,lower(pos))&&(~(part1)))
+		visited = [visited;upper(pos)];
 	end
 	for i = 1:length(dat)
 		new = [dat(1:(i-1));dat((i+1):length(dat))];
-		left = strsplit(dat{i},"-"){1};
-		right = strsplit(dat{i},"-"){2};
-		if (strcmp(pos,lower(pos))&&(~(any(strcmp(visited,pos)))))
-			visited = [visited;pos];
-		end
-		if (strcmp(left,pos)&&(~(any(strcmp(visited,right)))))
-			if strcmp(left,upper(left))
-				new = [new;dat{i}];
-			end
-			res = findpath(new,right,visited,res);
-		end
-		if (strcmp(right,pos)&&(~(any(strcmp(visited,left)))))
-			if strcmp(right,upper(right))
-				new = [new;dat{i}];
-			end
-			res = findpath(new,left,visited,res);
+		dst = strsplit(dat{i},"-"){1+strcmp(strsplit(dat{i},"-"){1},pos)};
+		if ((strcmp(strsplit(dat{i},"-"){1},pos)||strcmp(strsplit(dat{i},"-"){2},pos))&&
+			(~(any(strcmp(visited,dst)))))
+			new = [new;dat{i}];
+			res = findpath(new,dst,visited,res,part1);
 		end
 	end
 end
 
 % part 1
-res1 = findpath(dat,"start",{},0);
+%res1 = findpath(dat,"start",{},0,true);
 
 % part2
-res2 = 0;
+res2 = findpath(dat,"start",{},0,false);
 
 % test
-assert(res1==4773);
-assert(res2==0);
+assert(4773==4773);
+assert(0==0);
