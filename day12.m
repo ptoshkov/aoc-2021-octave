83df786183ca16d12bfcaa3be280c7697e5e299b
Pete Toshkov <petetoshkov@gmail.com>
2022-01-01 03:27:08 +0200

    improved day 12 solution

--- day12.m
+++ day12.m
@@ -8,29 +8,35 @@ function res = findpath(dat,pos,visited,res,part1)
 	if (strcmp(pos,"start")||
 			(strcmp(pos,lower(pos))&&
 				((sum(strcmp(cellfun("lower",visited,"UniformOutput",false),visited))>1)||part1)))
-		visited = [visited;pos];
+		visited(end+1) = pos;
 	elseif (strcmp(pos,lower(pos))&&any(strcmp(cellfun("lower",visited,"UniformOutput",false),pos))&&(~(part1)))
 		visited = cellfun("lower",visited,"UniformOutput",false);
 	elseif (strcmp(pos,lower(pos))&&(~(part1)))
-		visited = [visited;upper(pos)];
+		visited(end+1) = upper(pos);
 	end
 	for i = 1:length(dat)
-		new = [dat(1:(i-1));dat((i+1):length(dat))];
 		dst = strsplit(dat{i},"-"){1+strcmp(strsplit(dat{i},"-"){1},pos)};
 		if ((strcmp(strsplit(dat{i},"-"){1},pos)||strcmp(strsplit(dat{i},"-"){2},pos))&&
 			(~(any(strcmp(visited,dst)))))
-			new = [new;dat{i}];
+			% >>> OPTIMIZATION >>>
+			if (any(strcmp(visited,pos)))
+				new = [dat(1:(i-1));dat((i+1):length(dat))];
+			else
+				new = dat;
+			end
+			if strcmp(pos,"start")
+				new = new(cellfun("isempty",strfind(new,"start")));
+			end
+			% <<< OPTIMIZATION <<<
 			res = findpath(new,dst,visited,res,part1);
 		end
 	end
 end
 
-% part 1
-%res1 = findpath(dat,"start",{},0,true);
-
-% part2
+% parts 1 & 2
+res1 = findpath(dat,"start",{},0,true);
 res2 = findpath(dat,"start",{},0,false);
 
 % test
-assert(4773==4773);
-assert(0==0);
+assert(res1==4773);
+assert(res2==116985);
