90d4679cdf1b5788847294783ad5eb04764cb6b9
Pete Toshkov <petetoshkov@gmail.com>
2022-01-21 17:16:21 +0200

    added solution for day 20

--- day20.m
+++ day20.m
@@ -1,12 +1,12 @@
 dat = importdata("day20.dat");
 dat = strrep(dat,"#","1");
 dat = strrep(dat,".","0");
-alg = dat{1};
+keys = dat{1};
 map = "";
 for i = 2:(size(dat)(1))
 	map = [map;dat{i}];
 end
-alg = double(alg)-48;
+keys = double(keys)-48;
 map = double(map)-48;
 
 function output = pad_matrix(input,padding)
@@ -16,15 +16,30 @@ function output = pad_matrix(input,padding)
 	output = [padding*ones(HEIGHT+2,1),output,padding*ones(HEIGHT+2,1)];
 end
 
-% part 1
-for i = 1:50
-
-end
-
-% part 2
+% parts 1 & 2
 res1 = 0;
-res2 = 0;
+for i = 1:50
+	padding = (keys(1)*(~(keys(end)))*(rem(i,2)==0));
+	disp(i)
+	tic
+	map = pad_matrix(map,padding);
+	new = 0*ones(size(map));
+	pad = pad_matrix(map,padding);
+	for j = 1:length(map(:))
+		[row,col] = ind2sub(size(map),j);
+		row += 1; col += 1;
+		key = [pad(row-1,col-1),pad(row-1,col),pad(row-1,col+1),pad(row,col-1),pad(row,col),pad(row,col+1),pad(row+1,col-1),pad(row+1,col),pad(row+1,col+1)];
+		key = (bin2dec(num2str(key))+1);
+		new(j) = keys(key);
+	end
+	map = new;
+	if (i==2)
+		res1 = sum(map(:));
+	end
+	toc
+end
+res2 = sum(map(:));
 
 % test
-assert(res1==0);
-assert(res2==0);
+assert(res1==5400);
+assert(res2==18989);
